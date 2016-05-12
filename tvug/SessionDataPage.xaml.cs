32163fe600cf3e148aa66891f91e7693ef9a9bee
chris@rajapet.net <tardis12>
2016-05-12 09:48:28 -0400

    Initial check-in of module tvug

---
+++ tvug/SessionDataPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace tvug
+{
+	public partial class SessionDataPage : Xamarin.Forms.Pages.ListDataPage
+	{
+		public SessionDataPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
